f2019dae50efb68e83c64bc1db890bc442f0e879
SeungChan <cliwo@naver.com>
2016-04-27 16:04:46 +0900

    Second

--- JavaLab/src/Shape.java
+++ JavaLab/src/Shape.java
@@ -1,4 +1,30 @@
 
 public abstract class Shape {
 	
+	private int id;
+	
+	public Shape()
+	{
+		
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int i)
+	{
+		id=i;
+	}
+	public double calculateArea()
+	{
+		return 1.0;
+	}
+	public double calculatePerimeter()
+	{
+		return 1.0;
+	}
+	public void printfInfo()
+	{
+		System.out.println("ID : " + getId());
+	}
 }
